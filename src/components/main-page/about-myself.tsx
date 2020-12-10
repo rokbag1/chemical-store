79de508b23be8018f9ddc2f053e35dee08ef5ca9
Rokas <38216453+rokbag1@users.noreply.github.com>
2020-12-10 23:45:38 +0200

    merge

---
+++ src/components/main-page/about-myself.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome//free-brands-svg-icons";
+import Thinking from "../../styles/images/site-images/thinking.svg";
+
+function AboutMyself() {
+  return (
+    <div className="">
+      {/* <div className="menu-blocks">
+        
+      </div>
+      <div className="text">
+        <h2 className="cis-title">
+          Can chemistry make you a better programmer?
+        </h2>
+        <h5 className="about-page-text">Probably not :(</h5>
+        <h4 className="cis-subtitle">
+          But there are many more things you can find out interesting about this
+          web author.
+        </h4>
+        <div className="social-icons">
+          <div className="facebook">
+            <FontAwesomeIcon icon={faFacebook} />
+          </div>
+          <div className="instagram">
+            <FontAwesomeIcon icon={faInstagram} />
+          </div>
+          <div className="linkedin">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </div>
+          <div className="git">
+            <FontAwesomeIcon icon={faGithub} />
+          </div>
+        </div>
+        <div className="projects">
+          <h4 className="cis-subtitle">Some of my projects</h4>
+        </div>
+      </div>
+      <div className="image">
+        <img className="block-image" src={Thinking}></img>
+      </div> */}
+    </div>
+  );
+}
+export default AboutMyself;
